01005760cf78cf5b19709a73252435819ceadf12
letusbeus <letusbeus@mail.ru>
2024-10-31 22:39:27 +0300

    MyFirstTelegramBot: + /start + /bye + text + picture + smile + cat buttons

--- src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
@@ -8,6 +8,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
 import static com.javarush.telegrambot.BotData.BotName;
 import static com.javarush.telegrambot.BotData.BotToken;
 
@@ -23,9 +25,51 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update updateEvent) {
         // TODO: основной функционал бота будем писать здесь
-        sendTextMessageAsync("Hi!");
-        sendTextMessageAsync("*Hi!*"); // allows to use markdown, e.g. bold&cursive
-        sendTextMessageAsync("_Hi!_");
+//        sendTextMessageAsync("Hi!");
+//        sendTextMessageAsync("*Hi!*"); // allows to use markdown, e.g. bold&cursive
+//        sendTextMessageAsync("_Hi!_");
+        if (getMessageText().equals("/start")) {
+            sendTextMessageAsync("*Hi!*");
+        }
+
+        if (getMessageText().equals("/bye")) {
+            sendTextMessageAsync("_Goodbye!_");
+        }
+
+        if (getMessageText().equals("How are you?")) {
+            sendTextMessageAsync("I'm OK, thanx :)");
+        }
+
+        if (getMessageText().contains("bomb")) {
+            sendTextMessageAsync("*ALARM*");
+        }
+
+        if (getMessageText().contains("picture")) {
+            sendPhotoMessageAsync("step_8_pic");
+        }
+
+        if (getMessageText().contains("cat")) {
+            sendTextMessageAsync("*Choose your cat: *",
+                    Map.of(
+                            "Cat #1", "cat1",
+                            "Cat #2", "cat2",
+                            "Cat #3", "cat3",
+                            "Cat #4", "cat4",
+                            "Cat #5", "cat5"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("cat1")) {
+            sendPhotoMessageAsync("step_1_pic");
+        }
+
+        if (getCallbackQueryButtonKey().equals("cat2")) {
+            sendPhotoMessageAsync("step_2_pic");
+        }
+
+        if (getMessageText().equals("smile")) {
+            var message = getLastSentMessage();
+            editTextMessageAsync(message.getMessageId(), message.getText()+" \uD83D\uDE09 ");
+        }
     }
 
     public static void main(String[] args) throws TelegramApiException {
