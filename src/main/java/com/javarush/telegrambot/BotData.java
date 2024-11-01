692aa6dc435714b994f49f08f4a871a3acf8a92d
letusbeus <letusbeus@mail.ru>
2024-11-01 18:54:26 +0300

    Excluded BotData.java (added to .gitignore&removed from git)

--- src/main/java/com/javarush/telegrambot/BotData.java
+++
@@ -1,6 +0,0 @@
-package com.javarush.telegrambot;
-
-public class BotData {
-    public static final String BotName = "jr_cat_marathon_bot";
-    public static final String BotToken = "7231078185:AAGf--rBgX8TNUdiNt8kii212F7wxtW25Qk";
-}
